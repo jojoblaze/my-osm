5ac01a31425b78b5a4eae633d26da8c733c85b1f
Matteo Dello Ioio <matteo.delloioio@4ward.it>
2019-06-10 15:24:44 +0200

    wip

--- azure/install-osm.sh
+++ azure/install-osm.sh
@@ -99,14 +99,16 @@ echo '*** Step 5: Install mod_tile ***'
 # mod_tile is an Apache module that is required to serve tiles. Currently no binary package is available for Ubuntu. We can compile it from Github repository.
 
 # First install build dependency.
-
+echo '* Install build dependency *'
 sudo apt-get install git autoconf libtool libmapnik-dev apache2-dev -y
 
+echo '* cloning mod_tile from GitHub *'
 git clone https://github.com/openstreetmap/mod_tile.git
 
 cd mod_tile/
 
 # Compile and install
+echo '* Compile and install *'
 ./autogen.sh
 ./configure
 make
@@ -125,33 +127,36 @@ cd openstreetmap-carto-4.21.1/
 
 carto project.mml > style.xml
 
-exit
+# exit
 
 
 # *** Step 7: Configuring renderd ***
 echo '*** Step 7: Configuring renderd ***'
+
+echo '* replacing values in renderd.conf *'
 # In the [default] section, change the value of XML and HOST to
 # XML=/home/osm/openstreetmap-carto-2.41.0/style.xml
 # HOST=localhost
 sed -i "s/XML=\/home\/osm\/openstreetmap-carto-4.21.1\/style.xml/XML=\/home\/osm\/openstreetmap-carto-4.21.1\/style.xml/g" /usr/local/etc/renderd.conf
 sed -i "s/HOST=tile.openstreetmap.org/HOST=localhost/g" /usr/local/etc/renderd.conf
 
-
 # In [mapnik] section, change the value of plugins_dir
 sed -i "s/plugins_dir=\/usr\/lib\/mapnik\/input\//plugins_dir=\/usr\/lib\/mapnik\/3.0\/input\//g" /usr/local/etc/renderd.conf
 
 
 # Install renderd init script by copying the sample init script.
+echo '* Install renderd init script by copying the sample init script *'
 sudo cp mod_tile/debian/renderd.init /etc/init.d/renderd
 
 # Grant execute permission
+echo '* Grant execute permission *'
 sudo chmod a+x /etc/init.d/renderd
 
+echo '* replacing values in init.d/renderd *'
 # Change the following variable in /etc/init.d/renderd file
 sed -i "s/DAEMON=\/usr\/bin\/\$NAME/DAEMON=\/usr\/local\/bin\/\$NAME/g" /etc/init.d/renderd
 sed -i "s/DAEMON_ARGS=\"\"/DAEMON_ARGS=\"-c \/usr\/local\/etc\/renderd.conf\"/g" /etc/init.d/renderd
 sed -i "s/RUNASUSER=www-data/RUNASUSER=osm/g" /etc/init.d/renderd
-
 
 
 sudo mkdir -p /var/lib/mod_tile
@@ -159,7 +164,9 @@ sudo mkdir -p /var/lib/mod_tile
 # sudo chown osm:osm /var/lib/mod_tile
 sudo chown $PostgreSQLUserName:$PostgreSQLUserName /var/lib/mod_tile
 
+
 # start renderd service
+echo '* start renderd service *'
 sudo systemctl daemon-reload
 
 sudo systemctl start renderd
@@ -192,6 +199,7 @@ sudo ln -s /etc/apache2/mods-available/mod_tile.load /etc/apache2/mods-enabled/
 # ModTileMissingRequestTimeout 30
 
 # Save and close the file. Restart Apache.
+echo '* Save and close the file. Restart Apache. *'
 sudo systemctl restart apache2
 
 
