3a01da71f1ec2645b55683f1c7d8dd07d3944de9
Jojo <matteo.delloioio@gmail.com>
2019-07-04 20:36:49 +0200

    wip

--- install-osm.sh
+++ install-osm.sh
@@ -4,29 +4,51 @@ OSMDBPassword=$2
 OSMDatabaseName=$3
 OSMRegion=$4
 
+# output coloring
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+NC='\033[0m' # No Color
+
+
 ./install-postgresql-10.sh $OSMUserName $OSMDBPassword
 
 if [[ $? > 0 ]]; then
-    echo "Something goes wrong in PostgreSQL installation"
+    echo -e "${RED}Something goes wrong in PostgreSQL installation.${NC}"
     exit 1
 else
-    echo "PostgreSQL installation successfully"
+    echo -e "${GREEN}PostgreSQL installation successfully.${NC}"
 fi
 
 ./install-postgresql-gis.sh $OSMUserName $OSMDBPassword $OSMDatabaseName $OSMRegion
 
 if [[ $? > 0 ]]; then
-    echo "Something goes wrong in PostgreGIS installation"
+    echo "${RED}Something goes wrong in PostgreGIS installation${NC}"
     exit 1
 else
-    echo "PostgreGIS installation successfully"
+    echo "${GREEN}PostgreGIS installation successfully${NC}"
 fi
 
 ./install-osm-tile-server.sh $OSMUserName
 
 if [[ $? > 0 ]]; then
-    echo "Something goes wrong in webserver configuration"
+    echo "${RED}Something goes wrong in webserver configuration${NC}"
     exit 1
 else
-    echo "webserver configuration successfully"
+    echo "${GREEN}webserver configuration successfully${NC}"
 fi
